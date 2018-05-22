15b69ebf2f685864ec78deecf0b9cf25b1f813bf
OYM-DEV08 <OYM-DEV08@192.168.0.27>
2018-05-22 08:34:43 -0400

    modificacion de los navegadores

--- public_html/funciones.js
+++ public_html/funciones.js
@@ -1,17 +1,20 @@
-function mostrarvertical(){
-    document.getElementById('vertical').style.display='block';
-    document.getElementById('horizontal').style.display='none';
-    document.getElementById('colapsado').style.display='none';
+function mostrarvertical() {
+    document.getElementById('vertical').style.display = 'block';
+    document.getElementById('horizontal').style.display = 'none';
+    document.getElementById('colapsado').style.display = 'none';
+    document.getElementById('contenido').style.margin='-245px 0px 0px 20%';
 }
 /*esta funcion lo que realiza es mostrar un navegador que esta oculto que es el vertical y a la ves oculta el navegador horizontal y vertical*/
-function mostrarhorizontal(){
-    document.getElementById('vertical').style.display='none';
-    document.getElementById('horizontal').style.display='block';
-    document.getElementById('colapsado').style.display='none';
+function mostrarhorizontal() {
+    document.getElementById('vertical').style.display = 'none';
+    document.getElementById('horizontal').style.display = 'block';
+    document.getElementById('colapsado').style.display = 'none';
+    document.getElementById('contenido').style.margin = '0';
 }
 /*es funcion lo que realiza es mostrar un navegador que esta oculto que es el horizontal y a la ves oculta el navegador vertical y colapsado */
-function mostrarcolapsado(){
-    document.getElementById('vertical').style.display='none';
-    document.getElementById('horizontal').style.display='none';
-    document.getElementById('colapsado').style.display='block';
+function mostrarcolapsado() {
+    document.getElementById('vertical').style.display = 'none';
+    document.getElementById('horizontal').style.display = 'none';
+    document.getElementById('colapsado').style.display = 'block';
+    document.getElementById('contenido').style.margin = '-320px 0px 0px 20%';
 }
